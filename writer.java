61bf65edc8e52ff2b34845040a5e6c62be61071c
ge-ko77 <geckontrv7@gmail.com>
2026-02-20 15:00:26 -0600

    hello wordling it

---
+++ writer.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class writer {
+    public static void main(String[] args) {
+        String name, dob;
+        int[] chronic = new int[9];
+        int[] vax = new int[22];
+        
+        Scanner userinput = new Scanner(System.in);
+        System.out.println("enter name:");
+        name = userinput.nextLine();
+        System.out.println(name);
+
+    }
+}
